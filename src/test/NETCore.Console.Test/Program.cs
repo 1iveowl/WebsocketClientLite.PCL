82a9aebfc8b62d17a6b09b52c4c547759ad2fe1a
1iveowl <liveowl@outlook.com>
2017-03-02 18:14:49 +0100

    Bug fix: now supports more than 126 character payloads

--- src/test/NETCore.Console.Test/Program.cs
+++ src/test/NETCore.Console.Test/Program.cs
@@ -76,20 +76,19 @@ class Program
             System.Console.WriteLine("Sending: Test Single Frame");
             await websocketClient.SendTextAsync("Test Single Frame");
 
+            //var strArray = new[] { "Test ", "multiple ", "frames" };
 
-            var strArray = new[] { "Test ", "multiple ", "frames" };
+            //await websocketClient.SendTextAsync(strArray);
 
-            await websocketClient.SendTextAsync(strArray);
-
-            await websocketClient.SendTextMultiFrameAsync("Start ", FrameType.FirstOfMultipleFrames);
-            await Task.Delay(TimeSpan.FromMilliseconds(200));
-            await websocketClient.SendTextMultiFrameAsync("Continue... #1 ", FrameType.Continuation);
-            await Task.Delay(TimeSpan.FromMilliseconds(300));
-            await websocketClient.SendTextMultiFrameAsync("Continue... #2 ", FrameType.Continuation);
-            await Task.Delay(TimeSpan.FromMilliseconds(150));
-            await websocketClient.SendTextMultiFrameAsync("Continue... #3 ", FrameType.Continuation);
-            await Task.Delay(TimeSpan.FromMilliseconds(400));
-            await websocketClient.SendTextMultiFrameAsync("Stop.", FrameType.LastInMultipleFrames);
+            //await websocketClient.SendTextMultiFrameAsync("Start ", FrameType.FirstOfMultipleFrames);
+            //await Task.Delay(TimeSpan.FromMilliseconds(200));
+            //await websocketClient.SendTextMultiFrameAsync("Continue... #1 ", FrameType.Continuation);
+            //await Task.Delay(TimeSpan.FromMilliseconds(300));
+            //await websocketClient.SendTextMultiFrameAsync("Continue... #2 ", FrameType.Continuation);
+            //await Task.Delay(TimeSpan.FromMilliseconds(150));
+            //await websocketClient.SendTextMultiFrameAsync("Continue... #3 ", FrameType.Continuation);
+            //await Task.Delay(TimeSpan.FromMilliseconds(400));
+            //await websocketClient.SendTextMultiFrameAsync("Stop.", FrameType.LastInMultipleFrames);
 
             //await websocketClient.CloseAsync();
 
