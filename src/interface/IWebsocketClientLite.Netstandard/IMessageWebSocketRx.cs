931ad4725787d8eac4109f4175c4037bce2ca720
Mohammed Safan <safan@inofty.com>
2021-09-23 22:57:19 +0530

    removed unwanted usings

--- src/interface/IWebsocketClientLite.Netstandard/IMessageWebSocketRx.cs
+++ src/interface/IWebsocketClientLite.Netstandard/IMessageWebSocketRx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Security;
-using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
