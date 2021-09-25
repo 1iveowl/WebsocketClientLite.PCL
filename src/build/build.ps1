2d5708429bb19828f76b017082a69cd2c6c6749e
Jasper Hedegaard Bojsen <jasperhb@outlook.com>
2021-09-25 11:58:02 +0200

    build scrip update

--- src/build/build.ps1
+++ src/build/build.ps1
@@ -3,7 +3,7 @@ param([string]$version)
 if ([string]::IsNullOrEmpty($version)) {$version = "0.0.1"}
 
 $msbuild = join-path -path "C:\Program Files\Microsoft Visual Studio\2022\PReview\MSBuild\Current\Bin" -childpath "msbuild.exe"
-&$msbuild ..\..\interface\IWebsocketClientLite.Netstandard\IWebsocketClientLite.Netstandard.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\interface\IWebsocketClientLite.Netstandard\IWebsocketClientLite.Netstandard.csproj /t:Build /p:Configuration="Release"
 &$msbuild ..\main\WebsocketClientLite.Netstandard20\WebsocketClientLite.Netstandard20.csproj /t:Build /p:Configuration="Release"
 
 Remove-Item .\NuGet -Force -Recurse
